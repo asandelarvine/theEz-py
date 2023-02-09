2de7b2d135e7fb6c806fad3aa4a53516a2f351d2
asandelarvine <asandelarvine@gmail.com>
2023-02-09 21:20:10 +0300

    drawers set

---
+++ components/HomeScreen.js
@@ -0,0 +1,16 @@
+// Homescreen.js
+import React from "react";
+import { Button, View, Text } from "react-native";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Text>Welcome To Ezana Pay</Text>
+      {/* <Button
+        title="Go to About"
+        onPress={() => navigation.navigate("About")}
+      /> */}
+    </View>
+  );
+}
